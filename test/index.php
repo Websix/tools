e622215aa40e18d1729ef93dce4506df8a581090
Rubens Fernandes <rubensdrk@gmail.com>
2015-08-31 09:41:53 -0300

    add teste

--- test/index.php
+++ test/index.php
@@ -1,9 +1,11 @@
 <?php
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use W6\Tools\ErrorAgent;
 
- $__agentConfig = array(
+$__agentConfig = array(
     'host' => 'mail.websix.com.br',
     'port' => '587',
     'email' => 'rubens@websix.com.br',
@@ -13,12 +15,8 @@ use W6\Tools\ErrorAgent;
 $__agentConfig['sitename'] = 'Teste Agent';
 $__agentConfig['from'] = array('rubens@websix.com.br' => 'ErrorAgent');
 $__agentConfig['to'] = array('rubens@websix.com.br' => 'Rubens');
-$__agentConfig['sendEmail'] = false;
+$__agentConfig['sendEmail'] = true;
 
-new ErrorAgent(); 
+new ErrorAgent();
 echo $__agentConfig['teste Error'];//erro pq a posição do array nao existe
 throw new \Exception('teste Exception');//teste
-
-
-
-
